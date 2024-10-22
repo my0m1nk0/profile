7083799fc873f15a75b0a721000a99fa09256b01
my0m1nk0 <myominko.msdev@gmail.com>
2024-10-22 21:11:00 +0630

    draw modify

---
+++ lib/view/main/components/drawer/personal_info.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../../res/constants.dart';
+import 'header_info.dart';
+
+class PersonalInfo extends StatelessWidget {
+  const PersonalInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
+        AreaInfoText(title: 'Contact', text: '+959977278158'),
+        AreaInfoText(title: 'Email', text: 'smyominko.dev@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@smy0m1nk0'),
+        AreaInfoText(title: 'Github', text: '@my0m1nk0'),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
+        SizedBox(
+          height: defaultPadding,
+        ),
+      ],
+    );
+  }
+}
